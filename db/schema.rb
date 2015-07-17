072a4b5e83f029988b5f47d79ac0020be78c3c9e
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-07-17 15:16:30 +0300

    Created Peep model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717101734) do
+ActiveRecord::Schema.define(version: 20150717121606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "peeps", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- spec/features/peep_feature_spec.rb
+++ spec/features/peep_feature_spec.rb
@@ -8,4 +8,15 @@ feature 'showing peeps' do
       expect(page).to have_link 'Add a peep'
     end
   end 
+
+  context 'after peeps have been created' do
+    before do
+        Peep.create text: 'This is the first ever peep!'
+    end
+
+    scenario 'showing one peep' do
+      visit root_path
+      expect(page).to have_content 'This is the first ever peep'
+    end
+  end
 end
