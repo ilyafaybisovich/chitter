633ff44e656b072bd3054bde91467eab74d98b92
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-07-19 12:14:23 +0300

    User can see the appropriate sign up/sign in/log out links

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -2,26 +2,35 @@ require 'rails_helper'
 
 feature 'user can sign up, sign in and log out' do
   context 'user is not signed in' do
-    xscenario 'sees the sign up and sign in links on the homepage' do
+    scenario 'sees the sign up and sign in links on the homepage' do
       visit root_path
       expect(page).to have_link 'Sign up'
       expect(page).to have_link 'Sign in'
     end
 
-    xscenario 'does not see the log out link on the homepage' do
+    scenario 'does not see the log out link on the homepage' do
       visit root_path
       expect(page).not_to have_link 'Log out'
     end
   end
 
   context 'user is signed in' do
-    xscenario 'does not see the sign up and sign in links on the homepage' do
+    before do
+      visit root_path
+      click_link 'Sign up'
+      fill_in 'Email', with: "test@test.com"
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+      click_button 'Sign up'
+    end
+
+    scenario 'does not see the sign up and sign in links on the homepage' do
       visit root_path
       expect(page).not_to have_link 'Sign up'
       expect(page).not_to have_link 'Sign in'
     end
 
-    xscenario 'sees the log out link on the homepage' do
+    scenario 'sees the log out link on the homepage' do
       visit root_path
       expect(page).to have_link 'Log out'
     end
